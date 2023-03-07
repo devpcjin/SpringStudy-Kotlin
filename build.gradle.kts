5c65f46be46921ad00d610d54918572a93035a68
pcjin22 <pcjin@blockchain.co.kr>
2023-03-07 16:05:13 +0900

    init: init new project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,48 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
+val jar: Jar by tasks
+val bootJar: BootJar by tasks
+
+bootJar.enabled = false
+jar.enabled = true
+
+buildscript {
+    var kotlinVersion = "1.4.20"
+
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-allopen:${kotlinVersion}")
+        classpath("org.jetbrains.kotlin:kotlin-noarg:${kotlinVersion}")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${kotlinVersion}")
+        classpath("org.springframework.boot:spring-boot-gradle-plugin:2.5.5")
+        classpath("io.spring.gradle:dependency-management-plugin:1.0.11.RELEASE")
+    }
+}
+
+repositories {
+    mavenCentral()
+}
 
 plugins {
-    id("org.springframework.boot") version "2.6.5"
-    id("io.spring.dependency-management") version "1.1.0"
+    id("org.springframework.boot") version "2.5.5"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.4.20"
     kotlin("plugin.spring") version "1.4.20"
     kotlin("plugin.jpa") version "1.4.20"
+    kotlin("plugin.allopen") version "1.4.20"
     kotlin("plugin.noarg") version "1.4.20"
+}
+
+allOpen {
+    annotation("javax.persistence.Entity")
+}
+
+noArg {
+    annotation("javax.persistence.Entity")
 }
 
 group = "com.example"
