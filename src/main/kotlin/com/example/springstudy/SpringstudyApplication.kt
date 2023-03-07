1b3753e2ec86b6e9d081d2309a49f04e4bc7058b
pcjin22 <pcjin@blockchain.co.kr>
2023-03-07 17:06:15 +0900

    feat: timestamp 추가 (JpaAuditing)

--- src/main/kotlin/com/example/springstudy/SpringstudyApplication.kt
+++ src/main/kotlin/com/example/springstudy/SpringstudyApplication.kt
@@ -2,8 +2,10 @@ package com.example.springstudy
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class SpringstudyApplication
 
 fun main(args: Array<String>) {
