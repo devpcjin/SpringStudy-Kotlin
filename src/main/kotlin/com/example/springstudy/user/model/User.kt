793dd7820700347bb3e94dc11478fe3a8d546982
pcjin22 <pcjin@blockchain.co.kr>
2023-03-07 17:00:08 +0900

    feat: User entity 생성

---
+++ src/main/kotlin/com/example/springstudy/user/model/User.kt
@@ -0,0 +1,19 @@
+package com.example.springstudy.user.model
+
+import com.example.springstudy.common.Auditing
+import org.hibernate.envers.Audited
+import javax.persistence.*
+
+@Entity
+@Audited
+class User : Auditing(){
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null
+
+    @Column(length = 100)
+    var email: String? = null
+
+    @Column(length = 100)
+    var password: String? = null
+}
