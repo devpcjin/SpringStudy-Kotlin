1f277790346d4c93e933b5a982d54c85aa34ec3a
pcjin22 <pcjin@blockchain.co.kr>
2023-03-14 09:18:10 +0900

    feat: AuthenticationProvider 구현

--- src/main/kotlin/com/example/springstudy/security/config/CustomAuthenticationProvider.kt
+++ src/main/kotlin/com/example/springstudy/security/config/CustomAuthenticationProvider.kt
@@ -1,29 +1,42 @@
 package com.example.springstudy.security.config
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException
 import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.AuthenticationException
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Component
 import kotlin.jvm.Throws
 
 @Component
-class CustomAuthenticationProvider {
+class CustomAuthenticationProvider: AuthenticationProvider {
 
-//    @Throws(AuthenticationException::class)
-//    override fun authenticate(authentication: Authentication): Authentication {
-//        val userName: String = authentication.name
-//        val password: String = authentication.credentials as String
-//
-//        if("jin" == userName && "1765" == password) {
-//            return UsernamePasswordAuthenticationToken(userName, password, arrayListOf())
-//        } else {
-//            throw AuthenticationCredentialsNotFoundException("Error in authentication!");
-//        }
-//    }
-//
-//    override fun supports(authenticationType: Class<*>): Boolean {
-//        return UsernamePasswordAuthenticationToken::class.java.isAssignableFrom(authenticationType)
-//    }
+    @Autowired
+    var userDetailsService: UserDetailsService? = null
+
+    @Autowired
+    var passwordEncoder: PasswordEncoder? = null
+
+    override fun authenticate(authentication: Authentication?): Authentication {
+
+        var userName: String = authentication!!.name
+        var password: String = authentication!!.credentials.toString()
+
+        var u: UserDetails = userDetailsService!!.loadUserByUsername(userName)
+
+        if(passwordEncoder!!.matches(password, u.password)) {
+            return UsernamePasswordAuthenticationToken(userName, password, u.authorities)
+        } else {
+            throw BadCredentialsException("Something went wrong!")
+        }
+    }
+
+    override fun supports(authentication: Class<*>?): Boolean {
+        return authentication!! == UsernamePasswordAuthenticationToken::class
+    }
 }
--- src/main/kotlin/com/example/springstudy/security/config/ProjectTestConfig.kt
+++ src/main/kotlin/com/example/springstudy/security/config/ProjectTestConfig.kt
@@ -2,25 +2,40 @@ package com.example.springstudy.security.config
 
 import com.example.springstudy.security.service.CustomUserDetails
 import com.example.springstudy.security.service.CustomUserDetailsService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder
 import org.springframework.security.crypto.password.NoOpPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder
 import org.springframework.security.provisioning.JdbcUserDetailsManager
 import javax.sql.DataSource
 
 @Configuration
-class ProjectTestConfig {
+class ProjectTestConfig: WebSecurityConfigurerAdapter() {
 
-    @Bean
-    fun userDetailsService(dataSource: DataSource): UserDetailsService{
-        return JdbcUserDetailsManager(dataSource)
+    @Autowired
+    var authenticationProvider: AuthenticationProvider? = null
+
+    override fun configure(auth: AuthenticationManagerBuilder?) {
+        auth!!.authenticationProvider(authenticationProvider)
     }
 
     @Bean
     fun passwordEncoder(): PasswordEncoder {
-        return NoOpPasswordEncoder.getInstance()
+        var encoders = HashMap<String, PasswordEncoder>()
+
+        encoders["noop"] = NoOpPasswordEncoder.getInstance()
+        encoders["bcrypt"] = BCryptPasswordEncoder()
+        encoders["scrypt"] = SCryptPasswordEncoder()
+
+        return DelegatingPasswordEncoder("bcrypt", encoders)
     }
 }
